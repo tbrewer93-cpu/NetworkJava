4d5bcc10b837cbafc59cae08fa51678ed1eb3675
Tobias Brewer <tbrewerecfc@googlemail.com>
2024-11-21 20:04:24 +0000

    Add files via upload

---
+++ Network.java
@@ -0,0 +1,34 @@
+package Networks;
+
+public class Node {
+	static void init(int i, int j, int w):
+        #X position, Y Position and Weight
+        int[] = iel; #Internal edge list
+        int ipl = -1 #Internal particle list
+
+        ###DEFAULTS
+        idx=-1 #Node Index = -1
+        edge_max=0 #Maximum edges = 0
+        
+        if(kwargs.__contains__('idx')):
+            self.idx=kwargs['idx'] #Edge ID
+        if(kwargs.__contains__('conn')):
+            self.edge_max=kwargs['conn'] #Connectivity
+}
+
+public class Network {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] Nlist;
+		int[] Elist;
+		int[] plist;
+		
+		int N=0;
+		int E=0;
+		int p=0;
+		
+		System.out.println("Hello World!");
+	}
+
+}
