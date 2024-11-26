89e5664ae2dcdafa68f7d52bc69e1944827171fc
Tobias Brewer <tbrewerecfc@googlemail.com>
2024-11-26 17:03:25 +0000

    Add files via upload

--- Network.java
+++ Network.java
@@ -1,34 +1,68 @@
 package Networks;
-
-public class Node {
-	static void init(int i, int j, int w):
-        #X position, Y Position and Weight
-        int[] = iel; #Internal edge list
-        int ipl = -1 #Internal particle list
-
-        ###DEFAULTS
-        idx=-1 #Node Index = -1
-        edge_max=0 #Maximum edges = 0
-        
-        if(kwargs.__contains__('idx')):
-            self.idx=kwargs['idx'] #Edge ID
-        if(kwargs.__contains__('conn')):
-            self.edge_max=kwargs['conn'] #Connectivity
-}
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Network {
-
+	static void addNode(ArrayList<Node> Nlist, int i, int j) {
+		System.out.println("Hello Node!");
+		Nlist.add(new Node(i,j));
+	}
+	
+	static void moveNode(ArrayList<Node> Nlist, int i, int j, int idx) {
+		System.out.println("Node moved");
+		Nlist.get(0).x=i;
+		Nlist.get(0).y=j;
+	}
+	
+	static void deleteNode(ArrayList<Node> Nlist, int idx) {
+		Nlist.remove(0);
+		Nlist.add(0,null);
+		System.out.println("Node removed");
+	}
+		
+	static void printn(ArrayList<Node> Nlist) {
+		String nodes;
+		int N=Nlist.size();
+		for(int a=0; a<N; a++)
+		{
+			System.out.println(Nlist.get(a).string());
+		}
+	}
+	
+	static void init(ArrayList<Node> Nlist, Node[] Elist, Node[] plist) {
+		System.out.println("Run!!!");
+		boolean run = true;
+		Scanner ScObj = new Scanner(System.in);
+		while (run) {
+			System.out.println("Running");
+			String cmd = ScObj.nextLine();
+			if (cmd.equals("end")) {
+				run = false;
+			}
+			if (cmd.equals("node")) {
+				addNode(Nlist, 0, 0);
+			}
+			if (cmd.equals("printn")) {
+				printn(Nlist);
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] Nlist;
-		int[] Elist;
-		int[] plist;
+		ArrayList<Node> Nlist = new ArrayList<>();
+		Node[] Elist = {};
+		Node[] plist = {};
 		
 		int N=0;
 		int E=0;
 		int p=0;
 		
 		System.out.println("Hello World!");
+		init(Nlist, Elist, plist);
+		//moveNode(Nlist, 1, 4, 0);
+		//deleteNode(Nlist, 0);
+		System.out.println("End!");
 	}
 
 }
